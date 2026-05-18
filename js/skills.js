cb605809bedac4f5a2fa6624256481fe5b134b5a
Maksim <mksmvnv@icloud.com>
2026-05-18 23:31:25 +0300

    feat: add skill renderer

---
+++ js/skills.js
@@ -0,0 +1,27 @@
+const skills = [
+    "python",
+    "fastapi",
+    "aiohttp",
+    "postgres",
+    "alembic",
+    "redis",
+    "docker",
+    "git",
+    "linux",
+    "ci/cd",
+];
+
+function renderSkills(skillsArray) {
+    const skillsGrid = document.getElementsByClassName('skills-grid')[0];
+    
+    skillsArray.forEach(skill => {
+        const skillsCard = document.createElement('div');
+        skillsCard.className = 'skills-card';
+        skillsCard.innerHTML = `<p>${skill}</p>`;
+        skillsGrid.appendChild(skillsCard);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    renderSkills(skills);
+});
